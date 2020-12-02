00a44d084dad63517152605f03e24fe60ec6976c
steve <meadsteve@gmail.com>
2020-12-02 21:11:05 +0100

    dodgy day one part two

--- src/day_one/part_one.cr
+++ src/day_one/part_one.cr
@@ -1,9 +1,9 @@
-def input_numbers()
-  File.open("./src/day_one/input.txt").each_line().map(&.to_i)
+def input_numbers
+  File.open("./src/day_one/input.txt").each_line.map(&.to_i)
 end
 
-result = input_numbers().map{ |x|
-    input_numbers().select{|y| x + y == 2020}.map{|y| {x, y, x * y}}.to_a
-}.to_a()
+result_one = input_numbers().map { |x|
+  input_numbers().select { |y| x + y == 2020 }.map { |y| {x, y, x * y} }.to_a
+}.to_a
 
-puts result
+puts result_one
