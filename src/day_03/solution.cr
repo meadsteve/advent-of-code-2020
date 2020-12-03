8a8493ea626b1b34ffa6b03082487c95793f9fde
steve <meadsteve@gmail.com>
2020-12-03 21:25:57 +0100

    day 3 part 2

--- src/day_03/solution.cr
+++ src/day_03/solution.cr
@@ -1,17 +1,18 @@
-
 class WrappingString
-    @length : Int32
-    def initialize(@value : String)
-        @length = @value.size
-    end
+  @length : Int32
 
-    def [](position : Int)
-        @value[position % @length]
-    end
+  def initialize(@value : String)
+    @length = @value.size
+  end
+
+  def [](position : Int)
+    @value[position % @length]
+  end
 end
 
 struct Vector
   property x, y
+
   def initialize(@x : Int32, @y : Int32)
   end
 end
@@ -23,19 +24,36 @@ end
 TREE = '#'
 
 def count_tress(hill, movement : Vector)
-    position = Vector.new(0, 0)
-    trees = 0
-    hill.each { |slice|
-      if slice[position.x] == TREE && (position.y % movement.y == 0)
-         trees += 1
-      end
-      position.x += movement.x
-      position.y += movement.y
-    }
-    trees
+  x = 0
+  trees : Int64 = 0
+  range = 0..hill.size - 1
+  range.step(movement.y) { |y|
+    if hill[y][x] == TREE
+      trees += 1
+    end
+    x += movement.x
+  }
+  trees
 end
 
 input_hill = input_data().to_a
 
 # part one
 puts(count_tress(input_hill, Vector.new(3, 1)))
+
+# part two
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+part_two = [
+  count_tress(input_hill, Vector.new(1, 1)),
+  count_tress(input_hill, Vector.new(3, 1)),
+  count_tress(input_hill, Vector.new(5, 1)),
+  count_tress(input_hill, Vector.new(7, 1)),
+  count_tress(input_hill, Vector.new(1, 2)),
+]
+
+puts(part_two)
+puts(part_two.reduce { |acc, n| acc * n })
