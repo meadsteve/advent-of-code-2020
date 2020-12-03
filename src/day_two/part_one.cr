ad38411ce421a036e8a8f0eaef5b4e13b63cfd4c
steve <meadsteve@gmail.com>
2020-12-03 07:41:56 +0100

    day two part one soln

--- src/day_two/part_one.cr
+++ src/day_two/part_one.cr
@@ -1,18 +1,25 @@
+def input_data
+  File.open("./src/day_two/input.txt").each_line.map{|raw| parse_line(raw)}
+end
 
 struct Line
-  property min, max, letter, password
+  property min, max, required_letter, password
 
-  def initialize(@min : Int32, @max : Int32, @letter : Char, @password : String)
+  def initialize(@min : Int32, @max : Int32, @required_letter : Char, @password : String)
   end
 end
 
 def parse_line(raw_input)
-   match = raw_input.match(/([0-9])+-([0-9])+ ([a-zA-Z]): (.+)/)
+   match = raw_input.match(/([0-9]+)-([0-9]+) ([a-zA-Z]): (.+)/)
    if !match
-     raise "OH NO!"
+     raise "bad input: " + raw_input
    end
    Line.new(match[1].to_i, match[2].to_i, match[3][0], match[4])
 end
 
+def validate(line : Line)
+    occurances = line.password.count(line.required_letter)
+    occurances >= line.min && occurances <= line.max
+end
 
-puts(parse_line("1-3 a: abcde"))
+puts(input_data().select { |line| validate(line)}.size)
