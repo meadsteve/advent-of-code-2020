2d0bc635d8f3b6b88ef325a37b2db66ce3c7177d
steve <meadsteve@gmail.com>
2020-12-07 21:18:27 +0100

    formatting

--- src/day_05/run.cr
+++ src/day_05/run.cr
@@ -1,8 +1,8 @@
 require "./solution.cr"
 
 seat_numbers = input_data
-                   .map{ |raw_row| SeatingPosition.new(raw_row).seat_id}
-                   .to_a
+  .map { |raw_row| SeatingPosition.new(raw_row).seat_id }
+  .to_a
 
 part_one = seat_numbers.max
 puts part_one
